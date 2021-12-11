1747daa566eb8a65d5aa1af65443622f2e4ecb79
Elena Velez Nicolas <92687197+VNelena@users.noreply.github.com>
2021-12-11 18:59:09 +0000

    elena:adding products website, js, css and images

---
+++ app.js
@@ -0,0 +1,18 @@
+function changeTitle(){
+document.getElementById("seasonal").innerHTML = "It's Christmas time! "}
+
+/*/validating cust input doesn't work
+function validate() {
+  let x = document.forms["customForm"]["first-name"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+  else{
+   var confirm= alert("Thank you for your interest in Ilume Candles! We will let you know when your candle is ready");}
+}
+*/
+
+
+function validate() {
+ var confirm= alert("Thank you for your interest in Ilume Candles! We will let you know when your candle is ready");}

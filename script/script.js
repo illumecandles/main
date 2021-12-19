d38955de9f7e6876d8cbef9aea5d7af981aeb05e
Donal Munnelly <donal.munnelly@gmail.com>
2021-12-19 21:21:56 +0000

    image resize

--- script/script.js
+++ script/script.js
@@ -48,7 +48,7 @@ function formChecker(){
 		
 	}
 	
-function validateemail()  {  
+function //  {  
 var x=document.myform.email.value;  
 var atposition=x.indexOf("@");  
 var dotposition=x.lastIndexOf(".");  
@@ -57,6 +57,17 @@ if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){
   return false;  
   }  
 }  
+
+//validating email address, Information sourced from: https://www.javatpoint.com/javascript-form-validation
+function validateemail() {
+var x=document.myform.email.value;
+var atposition=x.indexOf("@");
+var dotposition=x.lastIndexOf(".");
+if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){
+alert("Please enter a valid e-mail address \n atpostion:"+atposition+"\n dotposition:"+dotposition);
+return false;
+}
+}
 function changemessage() {
   document.getElementById("submsg").innerHTML = "One of our team will be in touch soon!";
 }	

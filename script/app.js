04b19328d4807da8e844507eecf248f4f44bbcca
Elena Velez Nicolas <92687197+VNelena@users.noreply.github.com>
2021-12-19 21:47:33 +0000

    last changes product page

--- script/app.js
+++ script/app.js
@@ -12,8 +12,19 @@ document.getElementById("seasonal").innerHTML = "It's Christmas time! "}
  	var scent=document.getElementById("cust-scent").value;
  	var name=document.getElementById("cust-name").value;
  	var email=document.getElementById("cust-email").value;
- 	alert("Thank you for your interest in Ilume Candles" +name+ "! We will let you know when your candle is ready");
+ 	alert("Thank you for your interest in Ilume Candles " +name+ "! We will let you know when your candle is ready");
 
  	}
 
+
+//validating email address, Information sourced from: https://www.javatpoint.com/javascript-form-validation
+function validateemail() {
+var x=document.CustForm.email.value;
+var atposition=x.indexOf("@");
+var dotposition=x.lastIndexOf(".");
+if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){
+alert("Please enter a valid e-mail address \n atpostion:"+atposition+"\n dotposition:"+dotposition);
+return false;
+}
+}
 
